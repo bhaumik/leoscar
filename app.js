f9ac513072ed0ba0ea60efa3be66e2967eb5dfc3
Bhaumik Patel <bhaumik@thinkful.com>
2016-02-28 22:32:12 -0500

    Maybe if I want to add the Nos back

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ $(document).ready(function(){
 		var newSource = "assets/yes" + randomNumber + ".gif";
 		$('#homegif').attr("src", newSource);
 	});
+	$('#sadrefresh').click(function(){
+		var randomNumber = Math.floor(Math.random() * 18) + 1;
+		var newSource = "assets2/crying" + randomNumber + ".gif";
+		$('#homegif').attr("src", newSource);
+	});
 });
 
