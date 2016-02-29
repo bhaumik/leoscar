2988b5e57d6627c539996b4dc65b00d881d3c5d4
Bhaumik Patel <bhaumik@thinkful.com>
2016-02-28 23:11:26 -0500

    What if he didn't win?

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('html').click(function(){
+	$('#refresh, img').click(function(){
 		var randomNumber = Math.floor(Math.random() * 12) + 1;
 		var newSource = "assets/yes" + randomNumber + ".gif";
 		$('#homegif').attr("src", newSource);
@@ -8,6 +8,7 @@ $(document).ready(function(){
 		var randomNumber = Math.floor(Math.random() * 18) + 1;
 		var newSource = "assets2/crying" + randomNumber + ".gif";
 		$('#homegif').attr("src", newSource);
+		$('h1').text("No. 😭");
 	});
 });
 
