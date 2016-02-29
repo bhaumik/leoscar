14a968d27295a929d326765d19d1559fb7b33818
Bhaumik Patel <bhaumik@thinkful.com>
2016-02-28 19:29:08 -0500

    Remove 3 hypeless gifs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#refresh, img').click(function(){
-		var randomNumber = Math.floor(Math.random() * 16) + 1;
+		var randomNumber = Math.floor(Math.random() * 13) + 1;
 		var newSource = "assets/yes" + randomNumber + ".gif";
 		$('#homegif').attr("src", newSource);
 	});
